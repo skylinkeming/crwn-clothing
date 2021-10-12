e60c397dc5e6eb4d1b89a5a667e69f973e459fd3
Steve <germinglin@yahoo.com.tw>
2021-10-12 23:16:01 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from 'react'
+import './App.css'
+import HomePage from './pages/homepage/homepage.component'
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+    <BrowserRouter>
+      <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+      </Switch>
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
-      className='background-image'
+      className="background-image"
     ></div>
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
@@ -16,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
