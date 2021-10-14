1cd2860d51413afb598ea3e52fb78e1393dc3f75
Steve <germinglin@yahoo.com.tw>
2021-10-15 01:07:19 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-
+import React from 'react'
+import './App.css'
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component.jsx'
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-      <Switch>
+      <BrowserRouter>
+        <Header />
+        <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/hats" component={ShopPage} />
-      </Switch>
+        </Switch>
       </BrowserRouter>
-
     </div>
   )
 }
