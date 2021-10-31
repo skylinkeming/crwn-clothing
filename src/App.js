e5d92e49b1287278e645d0b11d70f91af6e7b8f0
Steve <germinglin@yahoo.com.tw>
2021-10-31 18:49:16 +0800

    finish creating form and input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
-import { Switch, BrowserRouter, Route } from 'react-router-dom'
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component.jsx'
-import Header from './components/header/header.component.jsx'
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-pages/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={ShopPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
