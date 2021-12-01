ebd788ff3d87d246eff8cb56255beb09019e86a7
Steve <germinglin@yahoo.com.tw>
2021-12-01 20:47:59 +0800

    add console log

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,14 +5,13 @@ import logger from 'redux-logger'
 import rootReducer from './root-reducer'
 
 const middlewares = []
-
-if(process.env.NODE_ENV==='development'){
-    middlewares.push(logger);
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
 }
-
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export const persistor = persistStore(store)
 
-export default { store, persistor };
+export default { store, persistor }
